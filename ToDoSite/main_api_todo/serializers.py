c985fe9e3b9540cb3116be4126a6fe524fe975f9
Akella-56 <dokucaevnikolaj7009@gmail.com>
2024-06-29 23:05:53 +0800

    Update 0.01

---
+++ ToDoSite/main_api_todo/serializers.py
@@ -0,0 +1,18 @@
+# serializers.py
+from rest_framework import serializers
+from .models import User, Team, Task
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
